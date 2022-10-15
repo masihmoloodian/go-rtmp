8c1a4ad9b545083a2afeffe1afc2e90320a5e233
Masih Moloodian <masihmoloodian@gmail.com>
2022-10-15 13:29:25 +0330

    - feat: Add mux router

--- main.go
+++ main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	LoadAppConfig()
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	log.Println(fmt.Sprintf("Server listen on port: %v", AppConfig.ServicePort))
+	http.ListenAndServe(fmt.Sprintf(":%v", AppConfig.ServicePort), router)
+}
+
+func RegisterRoutes(router *mux.Router) {
+	
+}
